abd28140abee843a09a026f16917997b608a2afe
aazario <alejandroazario@gmail.com>
2021-11-06 18:58:26 -0300

    domain name is removed

--- scripts/auth.sh
+++ scripts/auth.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-[[ "$(curl -s "https://www.duckdns.org/update?domains=${DUCKDNS_DOMAIN}&token=${DUCKDNS_TOKEN}&txt=${CERTBOT_VALIDATION}")" = "OK" ]]
+[[ "$(curl -s "https://www.duckdns.org/update?domains=${DUCKDNS_DOMAIN//.duckdns.org/}&token=${DUCKDNS_TOKEN}&txt=${CERTBOT_VALIDATION}")" = "OK" ]]
